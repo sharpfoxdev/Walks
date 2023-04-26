546fc12caadbbeb6b672784987a83af2a5e66248
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-04-27 01:28:34 +0200

    updated appsetings with connection string and added dbcontext

---
+++ WalksAPI/Data/WalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Data {
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        // sets/collections of entities in a database
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
