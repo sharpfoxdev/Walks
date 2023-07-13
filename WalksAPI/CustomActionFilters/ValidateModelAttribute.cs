5f35752212aaa25040c1a96ce4f94b02f49e62f1
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-07-13 20:26:02 +0200

    this didnt commit last time

--- WalksAPI/CustomActionFilters/ValidateModelAttribute.cs
+++ WalksAPI/CustomActionFilters/ValidateModelAttribute.cs
@@ -1,4 +1,12 @@
-﻿namespace WalksAPI.CustomActionFilters {
-    public class ValidateModelAttribute {
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WalksAPI.CustomActionFilters {
+    public class ValidateModelAttribute : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            if (!context.ModelState.IsValid) {
+                context.Result = new BadRequestResult();
+            }
+        }
     }
 }
