8ec2efed77be2196e26d5e46a4d2816ca2886640
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-07-17 22:16:33 +0200

    created token repository

---
+++ WalksAPI/Repositories/ITokenRepository.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WalksAPI.Repositories {
+    public interface ITokenRepository {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
