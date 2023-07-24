77c0dd4b5a19cd2d3481aa848bec108019846c9d
sharpfoxdev <sharpfoxdevelopment@gmail.com>
2023-07-24 20:31:12 +0200

    get all regions UI

---
+++ Walks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Walks.UI.Models.DTO;
+
+namespace Walks.UI.Controllers {
+    public class RegionsController : Controller {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index() {
+
+            List<RegionDto> response = new List<RegionDto>();
+
+            try {
+                var client = httpClientFactory.CreateClient();
+                // base url should come ideally from appsettings.json
+                // (url can differ for debug and release)
+                var httpResponseMessage = await client.GetAsync("https://localhost:7275/api/regions");
+
+                //throws an exception if we dont get successful response
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception ex) {
+                // log the exception
+                throw;
+            }
+
+            return View(response);
+        }
+    }
+}
---
+++ Walks.UI/Models/DTO/RegionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Walks.UI.Models.DTO {
+    public class RegionDto {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
